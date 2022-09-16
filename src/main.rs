3b3c255b161a6c7edf5446b6d79036ad0c1f11d2
Chris Dugan <c.m.s.dugan@gmail.com>
2022-09-16 14:09:22 -0400

    Stub out CLI

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,45 @@ mod board;
 mod tree;
 mod solver;
 
+use clap::{App, SubCommand, Arg};
 use crate::board::board::{Board, Move};
 use crate::solver::solver::Solver;
 
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Tic Tac Toe Solver")
+		.about("Solver for Tic Tac Toe")
+		.subcommand(
+			SubCommand::with_name("solve")
+				.about("Solve Tic Tac Toe Position")
+				.arg(
+					Arg::with_name("Position")
+						.help("Tic Tac Toe Position")						
+				)
+            ).get_matches();
+    
+    if let Some(matches) = matches.subcommand_matches("solve") {
+        match matches.value_of("Position") {
+            Some(position) => {
+                match Board::from_position(position) {
+                    Ok(board) => {
+                        let mut solver = Solver::from_board(board);
+                        let next_moves = solver.get_next_moves();
+                        println!("Next moves: {:?}", next_moves);
+                    },
+                    Err(error) => {
+                        println!("Invalid Position: {}", error);
+                    }
+                }
+            },
+            None => {
+                println!("Needs a Position!");
+            }
+        }
+    } else {
+        println!("Invalid command!");
+    }
 }
 
 #[cfg(test)]
